d44cab756bdc25d1f409018e45518b988bdc42c2
DiegoEspinoza10 <diego.espinoza.sanchez@est.una.ac.cr>
2024-02-11 12:48:45 -0600

    adding typing functionality

--- src/ComponentsJS/RealInput.jsx
+++ src/ComponentsJS/RealInput.jsx
@@ -3,18 +3,30 @@ import '../ComponentsCss/RealInput.css';
 
 function RealInput() {
     const [text, setText] = useState("");
+    const [typing, setTyping] = useState(false);
 
     function handleTextChange(event) {
         setText(event.target.value);
     }
 
+    function handleTyping() {
+        setTyping(true);
+    }
+
+    function handleTypingStopped() {
+        setTyping(false);
+    }
+
     return (
         <div>
-            <p>Type whatever you want here: {text}</p>
-            <input className="InputBox" type="text" value={text} onChange={handleTextChange} />
+            <p>Type whatever you want here: {typing ? 'Typing...' : ''}</p>
+            <p>{text}</p>
+            <input className="InputBox" type="text" value={text} onChange={handleTextChange} 
+                onFocus={handleTyping} onBlur={handleTypingStopped} />
         </div>
     );
 }
 
 export default RealInput;
+
 
