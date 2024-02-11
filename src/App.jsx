ad1bb16b4d43e7f81ac2aea2d7e5ea193c1db250
DiegoEspinoza10 <diego.espinoza.sanchez@est.una.ac.cr>
2024-02-11 13:53:46 -0600

    Cleaning the app

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,20 @@ import Box from './ComponentsJS/Box'
 import Counter from './ComponentsJS/Counter'
 import Real from './ComponentsJS/RealInput'
 import Fetch from './ComponentsJS/Fetch'
+import Timer from './ComponentsJS/Timer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+      
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
       <Box></Box>
       <Counter></Counter>
       <Real></Real>
+      <Timer></Timer>
       <Fetch/>
-    </>
+      </div>
   )
 }
 
