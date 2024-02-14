e3f3d1604e337b6571abb048ba82dd86ca363790
DiegoEspinoza10 <diego.espinoza.sanchez@est.una.ac.cr>
2024-02-14 12:35:48 -0600

    adding props name

---
+++ src/ComponentsJS/PropsName.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+function PropsName({name}) {
+
+
+    return (
+        <div>
+            <p>Hello {name}</p>
+        </div>
+    );
+}
+
+export default PropsName;
+
+
+
