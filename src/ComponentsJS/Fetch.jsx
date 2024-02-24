2fdcdbeacdf871864356e77922d0337413dc007a
DiegoEspinoza10 <diego.espinoza.sanchez@est.una.ac.cr>
2024-02-24 09:58:20 -0600

    Changing the async data fetching

--- src/ComponentsJS/Fetch.jsx
+++ src/ComponentsJS/Fetch.jsx
@@ -1,22 +1,23 @@
 import React, { useState, useEffect } from 'react';
 
 const Fetch = () => {
+
   const [employees, setEmployees] = useState([]);
+  
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setEmployees(data);
-      });
+    const fetchData = async () => {
+      const data = await fetch('https://jsonplaceholder.typicode.com/users');
+      const parsedData = await data.json();
+      setEmployees(parsedData);
+    };
+    fetchData();
   }, []);
+
   return (
     <div>
         <h4>Data fetched from API</h4>
       {employees.map((employees) => (
-        <div key={employees.id}>
+        <div key={employees.email}>
           <p>{employees.name}</p>
           <p>{employees.email}</p>
         </div>
